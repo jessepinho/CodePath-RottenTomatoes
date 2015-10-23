528472624853e0e0e058d310dd8078afab2782d7
Jesse Pinho <jesse@jessepinho.com>
2015-10-23 15:19:42 -0500

    Load low-res poster first

--- RottenTomatoes/MovieDetailsViewController.m
+++ RottenTomatoes/MovieDetailsViewController.m
@@ -20,8 +20,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.posterImageView setImageWithURL:[self getPosterImageURL]];
     [self setTitle:[self getTitle]];
+    [self setUpPosterImage];
     self.synopsisLabel.text = self.movie[@"synopsis"];
     [self.synopsisLabel sizeToFit];
     self.synopsisLabelContainer.contentSize = CGSizeMake(self.synopsisLabel.bounds.size.width, self.synopsisLabel.bounds.size.height);
@@ -31,7 +31,17 @@
     return [NSString stringWithFormat:@"%@ (%@)", self.movie[@"title"], self.movie[@"year"]];
 }
 
-- (NSURL *)getPosterImageURL {
+- (void)setUpPosterImage {
+    [self.posterImageView setImageWithURL:[self getLowResPosterImageURL]];
+    [self.posterImageView setImageWithURL:[self getHighResPosterImageURL]];
+}
+
+- (NSURL *)getLowResPosterImageURL {
+    NSString *thumbnailPosterURL = self.movie[@"posters"][@"thumbnail"];
+    return [NSURL URLWithString:thumbnailPosterURL];
+}
+
+- (NSURL *)getHighResPosterImageURL {
     NSString *originalPosterURL = self.movie[@"posters"][@"original"];
     NSRange range = [originalPosterURL rangeOfString:@".*cloudfront.net/" options:NSRegularExpressionSearch];
     NSString *highResPosterURL = [originalPosterURL stringByReplacingCharactersInRange:range withString:@"https://content6.flixster.com/"];
